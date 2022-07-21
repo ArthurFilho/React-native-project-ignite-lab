a7751d64edb0d4bd0b7160a43f8a139121282bec
Arthur <peraarthur2018@gmail.com>
2022-07-20 21:02:05 -0300

    first screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import {NativeBaseProvider} from 'native-base'
+import {NativeBaseProvider, StatusBar} from 'native-base'
 import {THEME} from './src/styles/theme'
 import {SignIn} from './src/screens/Signin'
+import { Loading } from './src/components/Loading';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fonstLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+  
   return (
   <NativeBaseProvider theme={THEME}>
-  <SignIn/>
+    <StatusBar
+    barStyle='light-content'
+    backgroundColor='transparent'
+    translucent
+    />
+  {fonstLoaded ? <SignIn/> : <Loading/>}
   </NativeBaseProvider>
   );
 }
---
+++ src/components/button.tsx
@@ -0,0 +1,27 @@
+import { Button as ButtonNativeBase, IButtonProps, Heading } from 'native-base';
+
+type Props = {
+    tittle: string;
+}
+
+export function Button({tittle, ...rest } : Props) {
+    return (
+  
+  <ButtonNativeBase 
+  bg="green.700"
+  h={14}
+  fontSize="sm"
+  rounded="sm"
+  _pressed={{ bg: "gray.500"}}
+  {...rest}
+  >
+  
+
+    <Heading color="white" fontSize="sm">
+    {tittle}
+    </Heading>
+  
+  
+  </ButtonNativeBase>
+  );
+}
